e100a7858edc6195b50620f6584e062edc3a0aaf
Adam Charane <adamcharane@gmail.com>
2020-01-04 00:36:58 +0100

    Base class

--- main.py
+++ main.py
@@ -133,6 +133,32 @@ class Pipe:
 
         return t_point or b_point
 
+
+class Base:
+    IMG = BASE_IMG
+    VEL = 5
+    WIDTH = IMG.get_width()
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
     bird.draw(win)
